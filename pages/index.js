c7adacfa619aa7237efacd18c35fc358aec62aab
Ming-der Wang <mingderwang@gmail.com>
2018-05-02 15:09:00 +0800

    add redux from https://github.com/ooade/NextSimpleStarter

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-import Link from 'next/link'
-export default () => (
-   <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+import 'isomorphic-fetch'
+import React from 'react'
+import withRedux from 'next-redux-wrapper'
+
+import Fork from '../components/Fork'
+import Todo from '../components/Todo'
+
+import initStore from '../utils/store'
+
+class Index extends React.Component {
+	static async getInitialProps({ store }) {
+		// Adding a default/initialState can be done as follows:
+		// store.dispatch({ type: 'ADD_TODO', text: 'It works!' });
+		const res = await fetch(
+			'https://api.github.com/repos/ooade/NextSimpleStarter'
+		)
+		const json = await res.json()
+		return { stars: json.stargazers_count }
+	}
+
+	render() {
+		const { stars } = this.props
+		return (
+			<div>
+				<Fork stars={stars} />
+				<div>
+					<Todo />
+				</div>
+			</div>
+		)
+	}
+}
+
+export default withRedux(initStore)(Index)
