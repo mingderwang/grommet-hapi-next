a40135a2a289136d0d58e84f2a885b033d05db8c
Ming-der Wang <mingderwang@gmail.com>
2018-05-02 15:11:27 +0800

    formatter

--- components/Todo.js
+++ components/Todo.js
@@ -1,49 +1,49 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import { addTodo, removeTodo } from '../actions/todo'
-import TodoItem from './TodoItem'
+import { addTodo, removeTodo } from "../actions/todo";
+import TodoItem from "./TodoItem";
 
 class Todo extends React.Component {
-	state = {
-		text: ''
-	}
+  state = {
+    text: ""
+  };
 
-	addTodos = e => {
-		e.preventDefault()
+  addTodos = e => {
+    e.preventDefault();
 
-		this.props.addTodo(this.state.text)
-		this.setState({ text: '' })
-	}
+    this.props.addTodo(this.state.text);
+    this.setState({ text: "" });
+  };
 
-	removeTodo = todo => {
-		this.props.removeTodo(todo)
-	}
+  removeTodo = todo => {
+    this.props.removeTodo(todo);
+  };
 
-	render() {
-		return (
-			<div className="mdl-card mdl-shadow--2dp">
-				<form onSubmit={this.addTodos}>
-					<div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-						<input
-							type="text"
-							value={this.state.text}
-							onInput={e => this.setState({ text: e.target.value })}
-							className="mdl-textfield__input"
-							id="input"
-						/>
-						<label className="mdl-textfield__label" htmlFor="input">
-							What must be done?
-						</label>
-					</div>
-				</form>
+  render() {
+    return (
+      <div className="mdl-card mdl-shadow--2dp">
+        <form onSubmit={this.addTodos}>
+          <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input
+              type="text"
+              value={this.state.text}
+              onInput={e => this.setState({ text: e.target.value })}
+              className="mdl-textfield__input"
+              id="input"
+            />
+            <label className="mdl-textfield__label" htmlFor="input">
+              What must be done?
+            </label>
+          </div>
+        </form>
 
-				<ul>
-					{this.props.todos.map((todo, i) => (
-						<TodoItem key={i} todo={todo} remove={this.removeTodo} />
-					))}
-				</ul>
-				<style>{`
+        <ul>
+          {this.props.todos.map((todo, i) => (
+            <TodoItem key={i} todo={todo} remove={this.removeTodo} />
+          ))}
+        </ul>
+        <style>{`
 						form {
 							background: #fff;
 							padding: 10px;
@@ -72,11 +72,11 @@ class Todo extends React.Component {
 							transform: translateY(100px);
 						}
 					`}</style>
-			</div>
-		)
-	}
+      </div>
+    );
+  }
 }
 
 export default connect(({ todos }) => ({ todos }), { addTodo, removeTodo })(
-	Todo
-)
+  Todo
+);
