90e7f80786e6ad91eb1bdbc6a45c5eeb2a8d0e3c
Ming-der Wang <mingderwang@gmail.com>
2018-05-04 14:13:46 +0800

    add modules and redux environment

---
+++ modules/redux-config/epics.js
@@ -0,0 +1,5 @@
+import { combineEpics } from 'redux-observable'
+//import { epic as pickTimeEpic } from '~/pickTimePeriod'
+
+export default combineEpics(
+)
---
+++ modules/redux-config/initialState.js
@@ -0,0 +1,7 @@
+// this initial state is used for server-side rendering as well
+//import { initialState as counter } from '~/counter'
+
+// merge all initial states and export
+export default Object.assign({},
+//  counter,
+)
