9d718635886ada4e248d8563d2590dafc064b5bc
Ming-der Wang <mingderwang@gmail.com>
2018-05-05 00:27:13 +0800

    ok for counter

--- modules/redux-config/initialState.js
+++ modules/redux-config/initialState.js
@@ -1,10 +1,7 @@
 // this initial state is used for server-side rendering as well
-//import { initialState as counter } from '~/counter'
-import { initialState as nav } from "~/nav";
 import { initialState as counter } from '~/counter'
+
 // merge all initial states and export
-export default Object.assign(
-  {},
-  nav,
+export default Object.assign({},
   counter,
-);
+)
--- modules/redux-config/store.js
+++ modules/redux-config/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import epics from './epics'
-import StateLoader from "./stateLoader"
+import StateLoader from "~/shared/stateLoader"
 /*
 import StateLoader from "~/shared/stateLoader"
 import { createStore } from 'redux'
@@ -21,13 +21,13 @@ let store = null
 export default (reducer, initialState = {}) => {
   const epicMiddleware = createEpicMiddleware(epics);
   const enhancers = composeWithDevTools(applyMiddleware(epicMiddleware))
-  //if (!store) {
-     store = createStore(reducer, initialState, enhancers)
-  //  const stateLoader = new StateLoader();
-  //  store = createStore(reducer, stateLoader.loadState(), enhancers)
-  //  store.subscribe(() => {
-  //      stateLoader.saveState(store.getState());
-  //  })
-  // } 
+  if (!store) {
+    // store = createStore(reducer, initialState, enhancers)
+    const stateLoader = new StateLoader();
+    store = createStore(reducer, stateLoader.loadState(), enhancers)
+    store.subscribe(() => {
+        stateLoader.saveState(store.getState());
+    })
+  }
   return store
 }
