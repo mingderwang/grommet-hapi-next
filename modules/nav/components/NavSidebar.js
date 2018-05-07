c3de477ef074f01ae1f1a4cad97c299c18d4bc9e
Ming-der Wang <mingderwang@gmail.com>
2018-05-07 11:31:26 +0800

    animate ok

--- modules/nav/components/NavSidebar.js
+++ modules/nav/components/NavSidebar.js
@@ -2,22 +2,55 @@ import Animate from 'grommet/components/Animate';
 import Value from 'grommet/components/Value';
 import Button from 'grommet/components/Button';
 import Box from 'grommet/components/Box';
+import Sidebar from 'grommet/components/Sidebar';
+import Header from 'grommet/components/Header';
+import Menu from 'grommet/components/Menu';
+import Title from 'grommet/components/Title';
+import Anchor from 'grommet/components/Anchor';
+import Footer from 'grommet/components/Footer';
+import UserIcon from "grommet/components/icons/base/User";
+import Close from "grommet/components/icons/base/Close";
+import Split from "grommet/components/Split";
 
 export default ({ toggle = true, _toggle }) => (
 <Box pad={{"between": "medium"}}
   align='center'>
-  <Button label='Leave'
-    primary={true}
-    onClick={_toggle} />
-  <Animate enter={{"animation": "fade", "duration": 1000, "delay": 0}}
+  <Animate leave={{"animation": "slide-right", "duration": 1000, "delay": 0}}
+    leave={{"animation": "slide-left", "duration": 1000, "delay": 0}}
     visible={toggle}
     keep={true}>
     <Box direction='row'>
-      <Box colorIndex='light-2'
-        margin='medium'
-        pad='large'>
-        <Value value={1} />
-      </Box>
+    <Sidebar colorIndex='neutral-1'
+  fixed={false}
+  size='small'>
+  <Header pad='medium'
+    justify='left'>
+    <Title>
+      BiMAP 0.11.1
+    </Title>
+  <Button icon={<Close />}
+    primary={true}
+    onClick={_toggle} />
+  </Header>
+  <Box flex='grow'
+    justify='start'>
+    <Menu primary={true}>
+      <Anchor href='#'
+        className='active'>
+        First
+      </Anchor>
+      <Anchor href='#'>
+        Second
+      </Anchor>
+      <Anchor href='#'>
+        Third
+      </Anchor>
+    </Menu>
+  </Box>
+  <Footer pad='medium'>
+    <Button icon={<UserIcon />} />
+  </Footer>
+</Sidebar>
     </Box>
   </Animate>
 </Box>
